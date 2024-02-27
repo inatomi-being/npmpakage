66b3405e6e724da1ee8a548c875b7a13c534bc01
BEING\inatomi <inatomi@beingcorp.co.jp>
2024-02-27 15:53:40 +0900

    パッケージ完成

---
+++ 99.npm_module/index.js
@@ -0,0 +1,9 @@
+const hello = require("./lib/hello.js");
+const helloworld = require("./lib/helloworld.js");
+
+module.exports = {
+  hello,
+  helloworld,
+};
+
+console.log("npm_module_do_jsファイルを実行するとhelloと表示されます");
